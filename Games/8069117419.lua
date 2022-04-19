572142452e2af629d1f9545b8055206640a1e065
Trustsense <98208313+TrustsenseDev@users.noreply.github.com>
2022-04-19 15:01:49 +0300

    Update 8069117419.lua

--- Games/8069117419.lua
+++ Games/8069117419.lua
@@ -1,5 +1,13 @@
 if not game:IsLoaded() then game.Loaded:wait() end
 
+-- anti afk
+local vu = game:GetService('VirtualUser')
+game:GetService('Players').LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+end)
+
 -- variables
 local plrs = game:GetService("Players")
 local plr = plrs.LocalPlayer
